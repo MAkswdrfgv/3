79db9c9d5f4b4897713ebd79a216c29da14244f5
MAkswdrfgv <maksimuspiter@gmail.com>
2022-06-28 17:39:18 +0300

    add video- site page, change name sql: schema - schema1

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def connect_db():
 def init_db():
     with app.app_context():
         db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
+        with app.open_resource('schema1.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
@@ -94,7 +94,11 @@ def index():
 def info():
     return render_template('info.html')
 
+@app.route('/video/')
+def video():
+    return render_template('video.html')
+
 
 if __name__ == '__main__':
-    init_db()
+    #init_db()
     app.run(debug=True)
